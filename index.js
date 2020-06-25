a8bcd2d3b68ef444712ad52d21bf7e2a30fa6c06
Kirill <kirill.novoseletskii2003@gmail.com>
2020-06-25 12:04:25 +0300

    make it better

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const forgot = currGen.forgotPass()
 const stage = new Stage([emailScene, passScene, logMail, logPass, done, sendVidios, forgot])
 // Connect to mongoDB
 async function connectDB() {
-    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://Kirill:Users1234@telebot.lcjgv.mongodb.net/Users'
+    const mongoUri = 'mongodb+srv://Kirill:Users1234@telebot.lcjgv.mongodb.net/Users'
 
     await mongoose.connect(mongoUri, {
         useNewUrlParser: true, 
