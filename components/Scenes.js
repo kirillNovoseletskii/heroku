1178b2a4530ab958acf94c2a4eaa86aa190bc3cd
Kirill <kirill.novoseletskii2003@gmail.com>
2020-07-12 12:07:06 +0300

    Update Scenes.js

--- components/Scenes.js
+++ components/Scenes.js
@@ -54,8 +54,8 @@ class SceneGen{
             const n = userTo.n
             setTimeout(async () => {
                 const date = new Date()
-                console.log(date.getHours(), date.getMinutes(), date.getSeconds())
-                if (date.getHours()+3 === time && date.getMinutes() === 0 && date.getSeconds() === 0){
+                console.log(date.getHours()+3, date.getMinutes(), date.getSeconds())
+                if (date.getHours()+3 === time && date.getMinutes() === 10 && date.getSeconds() == 0){
                     console.log('Vidion n:', n)
                     msg.reply(config.get("CURS_DATA.links")[n])
                     await Users.findOneAndUpdate({_teleId: msg.message.from.id}, {n: n+1})
